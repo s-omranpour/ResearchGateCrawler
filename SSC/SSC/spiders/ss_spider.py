a8bf5495ab6179767d0e846c2b0cb33b83dbfccd
ce441-981-95105721 <soroush.333@gmail.com>
2020-06-12 15:40:30 +0430

    works fine except abstract

---
+++ SSC/SSC/spiders/ss_spider.py
@@ -0,0 +1,50 @@
+import scrapy
+import json
+
+base_url = 'https://www.semanticscholar.org'
+urls = ['https://www.semanticscholar.org/paper/The-Lottery-Ticket-Hypothesis%3A-Training-Pruned-Frankle-Carbin/f90720ed12e045ac84beb94c27271d6fb8ad48cf', 
+        'https://www.semanticscholar.org/paper/Attention-is-All-you-Need-Vaswani-Shazeer/204e3073870fae3d05bcbc2f6a8e263d9b72e776',
+        'https://www.semanticscholar.org/paper/BERT%3A-Pre-training-of-Deep-Bidirectional-for-Devlin-Chang/df2b0e26d0599ce3e70df8a9da02e51594e0e992']
+
+class SSSpider(scrapy.Spider):
+    name = 'SSSpider'
+
+    ## Xpaths
+    TITLE = "//h1/text()"
+    ABSTRACT = "//div[@class='text-truncator abstract__text text--preline']/text()"
+    YEAR = "//span[@data-selenium-selector='paper-year']/span/span/text()"
+    AUTHORS = "//span[@class='author-list']//span/a/span/span/text()"
+    REFERENCES = "//div[@class='citation-list__citations']//div/div/h2/a/@href"
+
+    def __init__(self, start_urls=urls, max_num=10, root='data/'):
+        self.crawled = set()
+        self.urls = start_urls
+
+        self.root = root
+        self.max_num = max_num
+        self.counter = 0
+
+
+    def start_requests(self):
+        for url in self.urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+
+    def parse(self, response):
+        if response.status == 200 and self.counter < self.max_num:
+            self.counter += 1
+            url = response.url
+            item = {
+                'id': url.split('/')[-1],
+                'title': response.xpath(self.TITLE).get(),
+                'abstract': response.xpath(self.ABSTRACT).extract_first(),
+                'year': response.xpath(self.YEAR).get(),
+                'authors': response.xpath(self.AUTHORS).getall(),
+                'references': [base_url + ref for ref in response.xpath(self.REFERENCES).getall()]
+            }
+            json.dump(item, open(self.root + item['id'] + '.json', 'w'))
+            self.crawled.update([url])
+            
+            for ref in item['references']:
+                if ref not in self.crawled:
+                    yield scrapy.Request(url=base_url + ref, callback=self.parse)
