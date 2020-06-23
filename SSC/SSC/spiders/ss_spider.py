74c7c63983e6fb50070c470bfb53ca10e23b8759
ce441-981-95105721 <soroush.333@gmail.com>
2020-06-23 23:41:00 +0430

    number of requests fixed

--- SSC/SSC/spiders/ss_spider.py
+++ SSC/SSC/spiders/ss_spider.py
@@ -1,5 +1,7 @@
 import scrapy
 import json
+import os
+import threading
 
 base_url = 'https://www.semanticscholar.org'
 urls = ['https://www.semanticscholar.org/paper/The-Lottery-Ticket-Hypothesis%3A-Training-Pruned-Frankle-Carbin/f90720ed12e045ac84beb94c27271d6fb8ad48cf', 
@@ -17,7 +19,8 @@ class SSSpider(scrapy.Spider):
     # AUTHORS = "//span[@class='author-list']//span/a/span/span/text()"
     REFERENCES = "//div[@class='citation-list__citations']//div/div/h2/a/@href"
 
-    def __init__(self, start_urls=urls, max_num=2000, root='data/'):
+    def __init__(self, start_urls=urls, max_num=9, root='data/'):
+        self.queue = []
         self.crawled = set()
         self.urls = start_urls
 
@@ -27,12 +30,14 @@ class SSSpider(scrapy.Spider):
 
     def start_requests(self):
         for url in self.urls:
+            self.queue.append(url)
             yield scrapy.Request(url=url, callback=self.parse)
 
 
     def parse(self, response):
-        if response.status == 200 and len(self.crawled) < self.max_num:
-            url = response.url
+        url = response.url
+        if response.status == 200 and url not in self.crawled:
+            
             item = {
                 'id': url.split('/')[-1],
                 'title': response.xpath(self.TITLE).get(),
@@ -43,10 +48,14 @@ class SSSpider(scrapy.Spider):
             }
             json.dump(item, open(self.root + item['id'] + '.json', 'w'))
             self.crawled.update([url])
-            
-            for ref in item['references']:
-                if ref not in self.crawled and len(self.crawled) < self.max_num:
-                    yield scrapy.Request(url=ref, callback=self.parse)
-                else:
-                    return
-        return
+            self.queue.remove(url)
+
+
+
+            for ref in item['references'][:3]:
+                if ref not in self.queue and ref not in self.crawled and len(self.queue) + len(self.crawled) <= self.max_num:
+                    self.queue.append(ref)
+
+            for url in self.queue:
+                yield scrapy.Request(url=url, callback=self.parse)
+                
